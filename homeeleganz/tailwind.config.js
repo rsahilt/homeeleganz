4b381c012f389caeb0e7a1205f3732cd0a2f5281
Navraj singh kaler <navrajkaler996@gmail.com>
2023-12-05 12:01:12 -0600

    header and home structure added

--- homeeleganz/tailwind.config.js
+++ homeeleganz/tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                lato: ["Lato", "sans-serif"],
+            },
+            colors: {
+                primary: {
+                    DEFAULT: "#FFBF69", // Replace this with your primary color hex code
+                },
+            },
+        },
+    },
+    plugins: [],
+};
