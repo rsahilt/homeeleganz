ce394141da3b5c1326c6eea12aaa3946696e22e8
Navraj singh kaler <navrajkaler996@gmail.com>
2023-12-06 09:58:09 -0600

    checkout completed

--- homeeleganz/routes/web.php
+++ homeeleganz/routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/list-view', function () {
     return view('list-view');
 });
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
