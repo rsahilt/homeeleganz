e78e2aa0b341bda210ad362eaa4b79a62d93fa82
iamsahilr <srajbhandari63@gmail.com>
2023-12-06 12:54:43 -0600

    review section in progress

--- homeeleganz/resources/views/details.blade.php
+++ homeeleganz/resources/views/details.blade.php
@@ -20,15 +20,41 @@
 
     <div class="prod-details-container">
         <div class="prod-desc-image">
-            <div class="main-img">
-
+            <div class="main-img box-shadow-details">
+                <img src="/images/sofa.jpeg" alt="sofa image">
             </div>
             <div class="small-img">
-                <div class="img1"></div>
-                <div class="img2"></div>
+                <div class="img1 box-shadow-details">
+                    <img src="/images/img-1.jpg" alt="sofa image">
+                </div>
+                <div class="img2 box-shadow-details">
+                    <img src="/images/img-2.jpg" alt="sofa image">
+                </div>
             </div>
         </div>
         <div class="details">
-
+            <h1>Product Name</h1>
+            <p class="cat-text">Category Name</p>
+            <p class="cat-text">Top Rated in Living Room</p>
+            <hr>
+            <p class="prod-info"><strong>Price:</strong> $599.99 </p>
+            <p class="prod-info"><strong>Brand:</strong> Ciello</p>
+            <p class="prod-info"><strong>Colour:</strong> Blue</p>
+            <p class="prod-info"><strong>Description:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <hr>
+            <strong><h2>About This Item</h2></strong>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
     </div>
+
+    <div class="review">
+        <h3>Product Reviews</h3>
+        <div class="review-box">
+            <div class="user-img"></div>
+            <div class="user-contents">
+                
+            </div>
+        </div>
+    </div>
+
+    
