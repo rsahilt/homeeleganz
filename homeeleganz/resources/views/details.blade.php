f37aa2155d2ca9b58a22e7e355adff8967f9c413
iamsahilr <srajbhandari63@gmail.com>
2023-12-07 23:54:35 -0600

    submit button for review

--- homeeleganz/resources/views/details.blade.php
+++ homeeleganz/resources/views/details.blade.php
@@ -102,9 +102,8 @@
                     class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
                     placeholder="Leave your review..." ></textarea>
             </div>
-            <button type="submit"
-                class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                Post comment
+            <button type="submit" class="text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                Post Review
             </button>
         </form>
     </div>
