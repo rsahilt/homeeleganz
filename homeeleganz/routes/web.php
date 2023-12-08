68656f79f4f6ba2e36f69dffc8476a8d24f21e96
iamsahilr <srajbhandari63@gmail.com>
2023-12-08 13:10:05 -0600

    home blade and index blade separated

--- homeeleganz/routes/web.php
+++ homeeleganz/routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\ProductController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('index');
 });
 
 Route::get('/products', [ProductController::class, 'index']);
