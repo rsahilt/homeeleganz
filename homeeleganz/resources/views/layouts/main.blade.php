694ce968e0606db1a48ab0946bbcaeaff7739e83
iamsahilr <srajbhandari63@gmail.com>
2023-12-09 13:41:44 -0600

    slider added in home page

--- homeeleganz/resources/views/layouts/main.blade.php
+++ homeeleganz/resources/views/layouts/main.blade.php
@@ -20,7 +20,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    @vite('resources/css/app.css')
+    <!-- @vite('resources/css/app.css') -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
