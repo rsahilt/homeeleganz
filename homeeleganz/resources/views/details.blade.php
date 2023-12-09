c4351b6a49af606e68f96b570f464180cbc68d6b
iamsahilr <srajbhandari63@gmail.com>
2023-12-09 09:50:20 -0600

    code repeatation reductions

--- homeeleganz/resources/views/details.blade.php
+++ homeeleganz/resources/views/details.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Product Details</title>
+    <title>Home Eleganz</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
