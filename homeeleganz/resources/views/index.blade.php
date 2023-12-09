a4aa35bb286a098232ba08b1324d06fa95f9a922
iamsahilr <srajbhandari63@gmail.com>
2023-12-09 00:06:15 -0600

    static testimonials added in index page

--- homeeleganz/resources/views/index.blade.php
+++ homeeleganz/resources/views/index.blade.php
@@ -42,7 +42,7 @@
                 @include('components.card')
             </div>
         </div>
-        <div id="shop-by-brand">
+        <!-- <div id="shop-by-brand">
             <h1 class="text-[1.5em] my-[1rem] ml-[1rem] tracking-wide">Shop by brand</h1>
             <div class="flex justify-around">
                 @include('components.card')
@@ -50,7 +50,71 @@
                 @include('components.card')
                 @include('components.card')
             </div>
+        </div> -->
+        
+        <div class="mx-auto text-center md:max-w-xl lg:max-w-3xl mt-9">
+            <h3
+                class="mb-6 text-3xl font-bold text-neutral-800 dark:text-neutral-200">
+                Testimonial
+            </h3>
         </div>
+
+
+        <!-- Container for the Testimonials -->
+        <div class="grid gap-6 text-center md:grid-cols-3 lg:gap-12 bg-gray-100 py-10 px-5">
+            <!-- First Testimonial -->
+            <div class="mb-12 md:mb-0">
+                <div class="mb-6 flex justify-center">
+                    <img
+                        src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(1).jpg"
+                        class="w-32 rounded-full shadow-lg dark:shadow-black/30" />
+                </div>
+                <h5 class="mb-2 text-xl font-semibold">Steve George</h5>
+                <h6 class="mb-2 font-semibold text-primary dark:text-primary-500">
+                Winnipeg
+                </h6>
+                <p class="mb-2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos
+                id officiis hic tenetur quae quaerat ad velit ab hic tenetur.
+                </p>
+            </div> <!--First testimonial closed-->
+
+            <div class="mb-12 md:mb-0">
+                <div class="mb-6 flex justify-center">
+                    <img
+                        src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(1).jpg"
+                        class="w-32 rounded-full shadow-lg dark:shadow-black/30" />
+                </div>
+                <h5 class="mb-2 text-xl font-semibold">Brent Scott</h5>
+                <h6 class="mb-2 font-semibold text-primary dark:text-primary-500">
+                Toronto
+                </h6>
+                <p class="mb-2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos
+                id officiis hic tenetur quae quaerat ad velit ab hic tenetur.
+                </p>
+            </div> <!--second testimonial closed-->
+
+
+            <div class="mb-12 md:mb-0">
+                <div class="mb-6 flex justify-center">
+                    <img
+                        src="https://tecdn.b-cdn.net/img/Photos/Avatars/img%20(1).jpg"
+                        class="w-32 rounded-full shadow-lg dark:shadow-black/30" />
+                </div>
+                <h5 class="mb-2 text-xl font-semibold">Victor Balogun</h5>
+                <h6 class="mb-2 font-semibold text-primary dark:text-primary-500">
+                Alberta
+                </h6>
+                <p class="mb-2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos
+                id officiis hic tenetur quae quaerat ad velit ab hic tenetur.
+                </p>
+            </div> <!--third testimonial closed-->
+
+        
+        </div><!-- Container for the Testimonials Closed-->
+
     </div>
 
 
