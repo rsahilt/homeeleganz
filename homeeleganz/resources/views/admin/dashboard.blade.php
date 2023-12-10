724572a234a652f124352b73621fe5d1c6aebdb0
iamsahilr <srajbhandari63@gmail.com>
2023-12-10 13:02:59 -0600

    aside for dashboard

--- homeeleganz/resources/views/admin/dashboard.blade.php
+++ homeeleganz/resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.main')
+@extends('layouts.app')
 @section('content')
-    <div class="flex flex-col  min-h-screen overflow-x-hidden overflow-y-auto bg-dark text-white" style="margin-top: 20px;">
+    <!-- <div class="flex flex-col  min-h-screen overflow-x-hidden overflow-y-auto bg-dark text-white" style="margin-top: 20px;">
         <div class="flex items-center justify-center flex-1 h-full p-4">
             <main>
                 <h1 class="text-3xl font-semibold">Admin Dashboard</h1>
@@ -11,7 +11,45 @@
                 @endforeach
             </main>
         </div>
+    </div> -->
+    <div class="dashboard w-full">
+    <aside>
+    <ul class="pl-5 pt-5">
+        <li>
+            <a href="#" class="block py-3 px-4 text-white text-lg">Dashboard</a>
+        </li>
+
+        <li>
+            <a href="#" class="block py-3 px-4 text-white text-lg">Inbox</a>
+        </li>
+
+        <li>
+            <a href="#" class="block py-3 px-4 text-white text-lg">Users</a>
+        </li>
+
+        <li>
+            <a href="#" class="block py-3 px-4 text-white text-lg">Products</a>
+        </li>
+
+        <li>
+            <a href="#" class="block py-3 px-4 text-white text-lg">Taxes</a>
+        </li>
+
+        <li>
+            <a href="#" class="block py-3 px-4 text-white text-lg">Categories</a>
+        </li>
+
+        <li>
+            <a href="#" class="block py-3 px-4 text-white text-lg">Reviews</a>
+        </li>
+        
+        <li>
+            <a href="#" class="block py-3 px-4 text-white text-lg">Orders</a>
+        </li>
+    </ul>
+</aside>
+        <main></main>
     </div>
 
-    
 @endsection
+
--- homeeleganz/resources/views/layouts/app.blade.php
+++ homeeleganz/resources/views/layouts/app.blade.php
@@ -12,16 +12,16 @@
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@500&display=swap" rel="stylesheet">
-
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@500&display=swap" rel="stylesheet">
     <!-- @vite('resources/css/app.css') -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
 </head>
 
@@ -81,9 +81,10 @@
             </div>
         </nav>
 
-        
+        <main class="dashboard-main">
             @yield('content')
-     
+        </main>
+            
     </div>
 </body>
 </html>
