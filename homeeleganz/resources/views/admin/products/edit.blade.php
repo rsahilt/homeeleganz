a83f73f96d5469ace4a95641d8aeea17a50f4ce1
iamsahilr <srajbhandari63@gmail.com>
2023-12-10 08:41:08 -0600

    fixed the update , create method for products crud

--- homeeleganz/resources/views/admin/products/edit.blade.php
+++ homeeleganz/resources/views/admin/products/edit.blade.php
@@ -3,9 +3,9 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
         <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">{{ $title }}</h2>
-        <form action="{{ route('update', ['id' => $product->id]) }}" novalidate 
+        <form action="{{ route('update', ['id' => $product->id]) }}" 
             method="POST" 
-            enctype="multipart/form-data">
+            enctype="multipart/form-data" novalidate>
             @csrf
             @method('PUT')
             <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
