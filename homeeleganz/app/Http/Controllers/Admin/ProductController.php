8a4e833555ef5fec322a20743b23762086466d10
iamsahilr <srajbhandari63@gmail.com>
2023-12-09 23:40:01 -0600

    create and store method for products - backend

--- homeeleganz/app/Http/Controllers/Admin/ProductController.php
+++ homeeleganz/app/Http/Controllers/Admin/ProductController.php
@@ -17,4 +17,51 @@ class ProductController extends Controller
         $title = "Upload New Cartoon";
         return view('/admin/products/create', compact('title'));
     }
+
+    /**
+     * Store a newly created cartoon in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $valid = $request->validate([
+            'name'=>'required|string|min:1|max:255',
+            'summary'=>'required|string|min:10',
+            'color'=>'required|string|min:1|max:255',
+            // 'image' => 'required|image|mimes:jpeg,png,gif',
+            'image' => 'image|mimes:jpeg,png,gif',
+            'material'=>'required|string|min:1|max:255',
+            'unit_price'=>'required|integer|min:20|max:5000',
+            'description'=>'required|string|min:10',
+            'brand'=>'required|integer|min:1930|max:2050',
+            'weight'=>'required|integer|min:10|max:5000',
+            'dimensions'=>'required|string|min:1|max:255',
+            'category_id'=>'required|string|min:1|max:255',
+        ]);
+        $product = Product::create($valid);
+        // if($file = $request->file('image')){
+        //     $filename = uniqid() . '_' . $file->getClientOriginalName();
+        //     Storage::disk('images')->put($filename,File::get($file));
+        //     $cartoon->image=$filename;
+        //     $cartoon->save();
+        // }
+        $product->save();
+        
+        if($product){
+            //set success flash message
+            $flash = [
+                'type'=>'success',
+                'message'=>'New product succesfully added'
+            ];
+        }else{
+            $flash = [
+                'type'=>'danger',
+                'message'=>'Failed to add the product! Try Again'
+            ];
+        }
+
+        return redirect('/admin/products/index')->with(['flash' => $flash]);
+    }
 }
