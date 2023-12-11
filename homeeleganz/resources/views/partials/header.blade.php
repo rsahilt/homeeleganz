4522907b0d3be9b707c0fa5777468949d163c524
iamsahilr <srajbhandari63@gmail.com>
2023-12-11 15:04:16 -0600

    search functionality working

--- homeeleganz/resources/views/partials/header.blade.php
+++ homeeleganz/resources/views/partials/header.blade.php
@@ -12,6 +12,7 @@
                 <div class="relative mb-4 flex w-full flex-wrap items-stretch pl-10">
                     <input
                     type="search"
+                    name="search"
                     class="relative m-0 block min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none transition duration-200 ease-in-out focus:z-[3] focus:border-primary focus:text-neutral-700 focus:shadow-[inset_0_0_0_1px_rgb(59,113,202)] focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:focus:border-primary"
                     placeholder="Search"
                     aria-label="Search"
