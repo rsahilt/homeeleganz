3a4006c5d4268138204af82df840a41f57a53f7c
Shishir Shrestha <shishir.shrestha9808@gmail.com>
2023-12-11 10:53:52 +0545

    working on create for users

--- homeeleganz/resources/views/admin/users/create.blade.php
+++ homeeleganz/resources/views/admin/users/create.blade.php
@@ -1,37 +1,116 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-    <h1>Add New User</h1>
-    <form action="#" method="POST">
-        @csrf
 
-        <div class="form-group">
-            <label for="name">Name:</label>
-            <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" required>
-            @error('name')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
 
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
-            @error('email')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
+<div class="dashboard w-full">
+    @include('partials.aside')
+    <main>
+        <section class="bg-white dark:bg-gray-900">
+            <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+                <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new user</h2>
+                <a href="/admin/users/" class="mb-5 inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-500 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-blue-600 transition-all">
+                    Back
+                </a>
+                
+                <form action="#" novalidate 
+                    method="POST" 
+                    enctype="multipart/form-data">
+                    @csrf
+                    <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                        <div class="w-full">
+                            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                            <input type="text" name="first_name" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type your first name" value="{{ old('first_name') }}">
 
-        <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="password" class="form-control" id="password" name="password" required>
-            @error('password')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
+                            @error('first_name')
+                                <span class="text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    
+                        <div class="w-full">
+                            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                            <input type="text" name="last_name" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type you last name" value="{{ old('last_name') }}">
+                            @error('last_name')
+                                <span class="text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div>
 
-        <button type="submit" class="btn btn-primary">Create User</button>
-        <a href="#" class="btn btn-default">Cancel</a>
-    </form>
+                        <div class="sm:col-span-2">
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input type="text" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Email" value="{{ old('color') }}">
+                            @error('email')
+                                <span class="text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="sm:col-span-2">
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Password" value="{{ old('password') }}">
+                            @error('password')
+                                <span class="text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div> 
+
+                        
+
+                        <div class="w-full">
+                            <label for="address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Country</label>
+                            <input type="text" name="address" id="address" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Country" value="{{ old('address') }}">
+                            @error('address')
+                                <span class="text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="w-full">
+                            <label for="city" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">City</label>
+                            <input type="text" name="city" id="city" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="city" value="{{ old('city') }}">
+                            @error('city')
+                                <span class="text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="w-full">
+                            <label for="province" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Province</label>
+                            <input type="text" name="province" id="province" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Province" value="{{ old('province') }}">
+                            @error('province')
+                                <span class="text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="w-full">
+                            <label for="postal_code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Postal Code</label>
+                            <input type="text" name="postal_code" id="postal_code" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Postal Code" value="{{ old('postal_code') }}">
+                            @error('postal_code')
+                                <span class="text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div> 
+
+                        <div class="sm:col-span-2">
+                            <label for="phone_number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
+                            <input type="text" name="phone_number" id="phone_number" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Phone Number" value="{{ old('phone_number') }}">
+                            @error('phone_number')
+                                <span class="text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div> 
+
+                       
+
+                        
+
+                        
+
+                       
+                            
+                    </div>
+                    <button class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-green-600 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-green-700 transition-all">
+                        Add user
+                    </button>
+
+                    
+                </form>
+            </div>
+        </section>
+    </main>
 </div>
+
+
 @endsection
