64a0cbd710268e91fb930e76b48ea27578d831d9
Arsh <arshdeepsandhu5028@gmail.com>
2023-12-12 12:25:38 -0500

    Added more values in registration page

--- homeeleganz/resources/views/auth/register.blade.php
+++ homeeleganz/resources/views/auth/register.blade.php
@@ -1,55 +1,50 @@
 @extends('layouts.main')
 @section('content')
-<div class="container mx-auto mt-10">
-    <div class="flex justify-center">
-        <div class="w-full max-w-md">
-            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div class="mb-4">
-                    <h1 class="text-2xl font-bold">{{ __('Register') }}</h1>
-                </div>
-
-                <form method="POST" action="{{ route('register') }}">
-                    @csrf
-
-                    <div class="mb-4">
-                        <label for="name" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Name') }}</label>
-                        <input id="name" type="text" class="form-input w-full @error('name') border-red-500 @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                        @error('name')
-                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
-                        @enderror
-                    </div>
-
-                    <div class="mb-4">
-                        <label for="email" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Email Address') }}</label>
-                        <input id="email" type="email" class="form-input w-full @error('email') border-red-500 @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                        @error('email')
-                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
-                        @enderror
-                    </div>
-
-                    <div class="mb-4">
-                        <label for="password" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Password') }}</label>
-                        <input id="password" type="password" class="form-input w-full @error('password') border-red-500 @enderror" name="password" required autocomplete="new-password">
-                        @error('password')
-                            <p class="text-red-500 text-xs italic">{{ $message }}</p>
-                        @enderror
-                    </div>
-
-                    <div class="mb-4">
-                        <label for="password-confirm" class="block text-gray-700 text-sm font-bold mb-2">{{ __('Confirm Password') }}</label>
-                        <input id="password-confirm" type="password" class="form-input w-full" name="password_confirmation" required autocomplete="new-password">
-                    </div>
-
-                    <div class="mb-6">
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            {{ __('Register') }}
-                        </button>
-                    </div>
-                </form>
-            </div>
+<div class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="bg-white p-4 rounded-lg shadow-lg max-w-sm w-full">
+        <div class="flex justify-center mb-4">
+            <span class="inline-block bg-gray-200 rounded-full p-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4"/></svg>
+            </span>
         </div>
+        <h2 class="text-xl font-semibold text-center mb-2">Create a new account</h2>
+        <p class="text-gray-600 text-center mb-4">Enter your details to register.</p>
+        <form>
+            <div class="mb-2">
+                <label for="fullName" class="block text-gray-700 text-xs font-semibold mb-1">Full Name *</label>
+                <input type="text" id="fullName" class="form-input w-full px-3 py-2 border rounded-lg text-sm text-gray-700 focus:ring-blue-500" required placeholder="James Brown">
+            </div>
+            <div class="mb-2">
+                <label for="email" class="block text-gray-700 text-xs font-semibold mb-1">Email Address *</label>
+                <input type="email" id="email" class="form-input w-full px-3 py-2 border rounded-lg text-sm text-gray-700 focus:ring-blue-500" required placeholder="hello@alignui.com">
+            </div>
+            <div class="mb-2">
+                <label for="phone" class="block text-gray-700 text-xs font-semibold mb-1">Phone Number</label>
+                <input type="tel" id="phone" class="form-input w-full px-3 py-2 border rounded-lg text-sm text-gray-700 focus:ring-blue-500" placeholder="123-456-7890">
+            </div>
+            <div class="mb-2">
+                <label for="city" class="block text-gray-700 text-xs font-semibold mb-1">City</label>
+                <input type="text" id="city" class="form-input w-full px-3 py-2 border rounded-lg text-sm text-gray-700 focus:ring-blue-500" placeholder="Your City">
+            </div>
+            <div class="mb-2">
+                <label for="postalCode" class="block text-gray-700 text-xs font-semibold mb-1">Postal Code</label>
+                <input type="text" id="postalCode" class="form-input w-full px-3 py-2 border rounded-lg text-sm text-gray-700 focus:ring-blue-500" placeholder="12345">
+            </div>
+            <div class="mb-4">
+                <label for="password" class="block text-gray-700 text-xs font-semibold mb-1">Password *</label>
+                <input type="password" id="password" class="form-input w-full px-3 py-2 border rounded-lg text-sm text-gray-700 focus:ring-blue-500" required placeholder="••••••••">
+                <p class="text-gray-600 text-xs mt-1">Must contain 1 uppercase letter, 1 number, min. 8 characters.</p>
+            </div>
+            <button type="submit" class="w-full bg-blue-500 text-white px-3 py-2 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 text-sm">Register</button>
+            <p class="text-gray-600 text-xs text-center mt-3">
+                By clicking Register, you agree to accept Apex Financial's
+                <a href="#" class="text-blue-500 hover:underline">Terms and Conditions</a>.
+            </p>
+        </form>
     </div>
 </div>
+
+
 
     
 
