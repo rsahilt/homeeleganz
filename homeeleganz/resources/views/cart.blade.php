5a341ea775cbf487b18c7fb3e9a1be30e0894b16
iamsahilr <srajbhandari63@gmail.com>
2023-12-12 00:03:11 -0600

    cart blade styling

--- homeeleganz/resources/views/cart.blade.php
+++ homeeleganz/resources/views/cart.blade.php
@@ -4,25 +4,23 @@
     <main class="main-container">
         <h1 class="cart-title">My Cart</h1>
         <div class="cart-container bg-gray-100 w-[40%] mx-auto">
+            <?php $totalPrice = 0; ?>
             @foreach($cart as $cartItem)
-            <!-- Cart item -->
-            <div class="cart-item">
-                <div class="cart-item-image-box"></div>
-                <div class="cart-item-details">
-                    <div class="room">
-                        <p class="item-name">{{ $cartItem['name'] }}</p>
-                        
-                    </div>
-                    <!-- <div class="item">
-                        <p class="item-quantity">Quantity: 1</p>
-                    </div> -->
-                    <div class="price">
-                        <p class="item-price">${{ $cartItem['unit_price'] }}</p>
-                        <button class="item-remove">Remove</button>
+                <!-- Cart item -->
+                <div class="cart-item">
+                    <div class="cart-item-image-box"></div>
+                    <div class="cart-item-details">
+                        <div class="room">
+                            <p class="item-name">{{ $cartItem['name'] }}</p>
+                        </div>
+                        <div class="price">
+                            <p class="item-price">${{ $cartItem['unit_price'] }}</p>
+                            <button class="item-remove">Remove</button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <hr>
+                <hr>
+                <?php $totalPrice += $cartItem['unit_price']; ?>
             @endforeach
         </div>
 
@@ -31,8 +29,8 @@
                 <p class="summary-items">Bag: {{ count($cart) }} items</p>
             </div>
             <div class="total">
-                <!-- Calculate total price here -->
-                <p class="summary-total">Total: $100</p>
+                <!-- Display the calculated total price -->
+                <p class="summary-total">Sub Total: ${{ $totalPrice }}</p>
                 <button class="summary-checkout">Checkout</button>
             </div>
         </div>
