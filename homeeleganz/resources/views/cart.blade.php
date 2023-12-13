ae26b211bc69f4f976c719105a8dfc082d95b842
Navraj singh kaler <navrajkaler996@gmail.com>
2023-12-13 09:25:24 -0600

    css changed

--- homeeleganz/resources/views/cart.blade.php
+++ homeeleganz/resources/views/cart.blade.php
@@ -1,52 +1,71 @@
 @extends('layouts.main')
 @section('content')
 
-    <main class="main-container">
-        <h1 class="cart-title">My Cart</h1>
-        <div class="cart-container bg-gray-100 w-[40%] mx-auto">
-            <?php $totalPrice = 0; ?>
-            @foreach($cart as $cartItem)
-                <!-- Cart item -->
-                <div class="cart-item">
-                    <div class="cart-item-image-box">
-                        <img src="/images/{{ $cartItem['image'] }}" alt="{{ $cartItem['name'] }}" >
-                    </div>
-                    <div class="cart-item-details">
-                        <div class="room">
-                            <p class="item-name">{{ $cartItem['name'] }}</p>
-                            <span>Quantity: 1</span>
-                        </div>
-                        <div class="price">
-                            <p class="item-price">${{ $cartItem['unit_price'] }}</p>
-                            <button class="item-remove">Remove</button>
-                        </div>
-                    </div>
+<div class="mt-[4rem] py-3    w-[60%] mx-auto">
+    <h1 class="cart-title">My Cart</h1>
+    <div class="cart-container bg-gray-100 mx-auto">
+        <?php $totalPrice = 0; ?>
+        @foreach($cart as $cartItem)
+        <!-- Cart item -->
+        <div class="cart-item">
+            <div class="cart-item-image-box">
+                <img src="/images/{{ $cartItem['image'] }}" alt="{{ $cartItem['name'] }}">
+            </div>
+            <div class="cart-item-details">
+                <div class="room">
+                    <p class="item-name">{{ $cartItem['name'] }}</p>
+                    <span>Quantity: 1</span>
                 </div>
-                <hr>
-                <?php $totalPrice += $cartItem['unit_price']; ?>
-            @endforeach
-        </div>
-
-        <div class="cart-summary">
-            <div class="summary">
-                <p class="summary-items">Bag: {{ count($cart) }} items</p>
-            </div>
-            <div class="total">
-                <!-- Display the calculated total price -->
-                <p class="summary-total">Sub Total: ${{ $totalPrice }}</p>
-                <p>GST: ${{ $totalGST }}</p>
-                <p>PST: ${{ $totalPST }}</p>
-                <p>HST: ${{ $totalHST }}</p>
-                <p>Total Taxes: ${{ $totalTaxes }}</p>
-                <strong><p>Total: ${{ $totalPriceWithTaxes }}</p></strong>
-                <form action="{{ route('checkout') }}" method="POST" novalidate>
-                    @csrf
-                    <input type="hidden" name="cart" value="{{ json_encode($cart) }}">
-                    <button type="submit" class="summary-checkout">Checkout</button>
-                </form>
+                <div class="price">
+                    <p class="item-price">${{ $cartItem['unit_price'] }}</p>
+                    <button class="item-remove">Remove</button>
+                </div>
             </div>
         </div>
-    </main>
+        <hr>
+        <?php $totalPrice += $cartItem['unit_price']; ?>
+        @endforeach
+    </div>
 
-    @include('partials.footer')
+    <div class="flex flex-col items-end">
+
+        <div class="text-right">
+            <!-- Display the calculated total price -->
+
+            <div class="flex justify-between gap-7  my-3">
+                <span class="subtotal-label"><strong>Bag:</strong> </span> <span class="subtotal-value">{{ count($cart) }}
+                    items</span>
+            </div>
+            <div class="flex justify-between gap-7  mb-3">
+                <span class="subtotal-label"><strong>Sub Total:</strong> </span> <span class="subtotal-value">${{ $totalPrice }}</span>
+            </div>
+
+            <div class="flex justify-between gap-7  mb-3 tax-line">
+                <span class="tax-label"><strong>GST:</strong> </span> <span class="tax-value">${{ $totalGST }}</span>
+            </div>
+            <div class="flex justify-between gap-7  mb-3 tax-line">
+                <span class="tax-label"><strong>PST:</strong></span> <span class="tax-value">${{ $totalPST }}</span>
+            </div>
+            <div class="flex justify-between gap-7  mb-3 tax-line">
+                <span class="tax-label"><strong>HST:</strong></span> <span class="tax-value">${{ $totalHST }}</span>
+            </div>
+            <div class="flex justify-between gap-7  mb-3">
+                <span class="tax-label"><strong>Total Taxes:</strong></span> <span class="tax-value">${{ $totalTaxes }}</span>
+            </div>
+
+            <div class="flex justify-between gap-7  mb-3 total-line">
+                <strong>Total:</strong> <span class="total-value">${{ $totalPriceWithTaxes }}</span>
+            </div>
+
+
+            <form action="{{ route('checkout') }}" method="POST" novalidate>
+                @csrf
+                <input type="hidden" name="cart" value="{{ json_encode($cart) }}">
+                <button type="submit" class="summary-checkout ">Checkout</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+@include('partials.footer')
 @endsection
