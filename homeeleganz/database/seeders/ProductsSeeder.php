3a8196e720205980a087dcefc896dcb0606d00c0
Arsh <arshdeepsandhu5028@gmail.com>
2023-12-13 19:37:41 -0500

    added products in productsSeeder for bedrooms

--- homeeleganz/database/seeders/ProductsSeeder.php
+++ homeeleganz/database/seeders/ProductsSeeder.php
@@ -13,18 +13,18 @@ class ProductsSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('products')->insert([
-            'name' => 'ErgoComfort Office Chair',
-            'image' => 'resized_office_1.jpg',
-            'summary' => 'A comfortable and ergonomic office chair designed for long hours of work.',
-            'color' => 'Black',
-            'material' => 'Mesh, PU Leather',
-            'unit_price' => '299.99',
-            'description' => 'The ErgoComfort Office Chair features adjustable lumbar support, padded armrests, and a breathable mesh back for optimal comfort during extended periods of sitting. The PU leather seat adds a touch of elegance while ensuring durability. Its ergonomic design promotes proper posture and reduces strain on the back and neck.',
-            'brand' => 'ComfortMax',
-            'weight' => '35',
-            'dimensions' => '24x26x45 inches',
-            'created_at' => date('m/d/Y'),
-        ]);
+        // DB::table('products')->insert([
+        //     'name' => 'ErgoComfort Office Chair',
+        //     'image' => 'resized_office_1.jpg',
+        //     'summary' => 'A comfortable and ergonomic office chair designed for long hours of work.',
+        //     'color' => 'Black',
+        //     'material' => 'Mesh, PU Leather',
+        //     'unit_price' => '299.99',
+        //     'description' => 'The ErgoComfort Office Chair features adjustable lumbar support, padded armrests, and a breathable mesh back for optimal comfort during extended periods of sitting. The PU leather seat adds a touch of elegance while ensuring durability. Its ergonomic design promotes proper posture and reduces strain on the back and neck.',
+        //     'brand' => 'ComfortMax',
+        //     'weight' => '35',
+        //     'dimensions' => '24x26x45 inches',
+        //     'created_at' => date('m/d/Y'),
+        // ]);
     }
 }
