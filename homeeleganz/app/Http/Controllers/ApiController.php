ed9820b19dc996826d055ceecf275dc82d49b27a
iamsahilr <srajbhandari63@gmail.com>
2023-12-15 11:55:46 -0600

    navraj helped with inserting into orders and line_items

--- homeeleganz/app/Http/Controllers/ApiController.php
+++ homeeleganz/app/Http/Controllers/ApiController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Response;
-
 use Illuminate\Http\Request;
-
 use Pacewdd\Bx\_5bx;
+use App\Models\Order;
+use App\Models\LineItem;
+
 
 use Auth;
 
@@ -27,38 +28,46 @@ class ApiController extends Controller
             ///inserting into order table
             $order = new Order();
             $order->order_date = now();
-            $order->sub_total = $valid['sub_total'];
-            $order->pst = $valid['pst']; 
-            $order->gst = $valid['gst'];
-            $order->hst = $valid['hst'];
-            $order->total = $valid['total'];
+            // $order->sub_total = $valid['sub_total'];
+            //$order->pst = session('totalGST'); 
+            $order->pst = session('totalPST'); 
+            $order->gst = session('totalGST');
+            $order->hst = session('totalHST');
+            $order->sub_total = session('totalPrice');
+            $order->total = session('totalPriceWithTaxes');
+
+            $userdetails = Auth::user();
             $order->user_id = Auth::id();
-            $order->address = $valid['address'];
-            $order->city = $valid['city'];
-            $order->province = $valid['province'];
-            $order->postal_code = $valid['postal_code'];
+            $order->address = $userdetails->address;
+            $order->city = $userdetails->city;
+            $order->province = $userdetails->province;
+            $order->postal_code = $userdetails->postal_code;
             
             $order->save(); //save order
+    
 
-            //inserting into line_items
-            $lineItem = new LineItem();
-            $lineItem->order_id = $order->id;
-            $lineItem->product_id = $valid['product_id']; 
-            $lineItem->name = $valid['name'];
-            $lineItem->quantity = $valid['quantity']; 
-            $lineItem->unit_price = $valid['unit_price']; 
-            
-            $lineItem->save(); //save line_items
+            $cartItems = session('cartItems');
+            foreach($cartItems as $cartItem){
+                // //inserting into line_items
+                $lineItem = new LineItem();
+                $lineItem->order_id = $order->id;
+                $lineItem->product_id = $cartItem->id; 
+                $lineItem->name = $cartItem->name;
+                $lineItem->quantity = 1; 
+                $lineItem->unit_price = $cartItem->unit_price;
+                $lineItem->save(); //save line_items
+            }
 
-            
+
+
             //transaction vbox
             $transaction = new _5bx(2257811, 'a88c8843898e4daad5646322ca06f22d');
-            // $transaction->amount($valid['totalamount']);
+
             $transaction->card_num($valid['cardNumber']);
             $transaction->exp_date($valid['expiry']);
             $transaction->cvv($valid['cvv']);
             $transaction->card_type($valid['cardType']);
-            $transaction->ref_num($valid['ref']);
+            $transaction->ref_num($order->id);
         
             $response = $transaction->authorize_and_capture(); // returns JSON object
         
@@ -68,7 +77,7 @@ class ApiController extends Controller
         }
 
         $cart = $request->session()->forget('cart');
-        return view('/invoice', compact('response','userdetails','cart'));
+        // return view('/invoice', compact('response','userdetails','cart'));
         
     }
     
