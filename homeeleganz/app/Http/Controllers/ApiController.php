98f9dfbb424dc152164f3dc28023931a27513e8a
iamsahilr <srajbhandari63@gmail.com>
2023-12-18 10:28:00 -0600

    orders and line items inserting even if not valid

--- homeeleganz/app/Http/Controllers/ApiController.php
+++ homeeleganz/app/Http/Controllers/ApiController.php
@@ -24,63 +24,63 @@ class ApiController extends Controller
             'expiry' => 'required|numeric|digits:4',
         ]);
         
-        if ($valid) {
-            ///inserting into order table
-            $order = new Order();
-            $order->order_date = now();
-            // $order->sub_total = $valid['sub_total'];
-            //$order->pst = session('totalGST'); 
-            $order->pst = session('totalPST'); 
-            $order->gst = session('totalGST');
-            $order->hst = session('totalHST');
-            $order->sub_total = session('totalPrice');
-            $order->total = session('totalPriceWithTaxes');
+        ///inserting into order table
+        $order = new Order();
+        $order->order_date = now();
+        // $order->sub_total = $valid['sub_total'];
+        //$order->pst = session('totalGST'); 
+        $order->pst = session('totalPST'); 
+        $order->gst = session('totalGST');
+        $order->hst = session('totalHST');
+        $order->sub_total = session('totalPrice');
+        $order->total = session('totalPriceWithTaxes');
 
-            $userdetails = Auth::user();
-            $order->user_id = Auth::id();
-            $order->address = $userdetails->address;
-            $order->city = $userdetails->city;
-            $order->province = $userdetails->province;
-            $order->postal_code = $userdetails->postal_code;
-            
-            $order->save(); //save order
-    
-
-            $cartItems = session('cartItems');
-            foreach($cartItems as $cartItem){
-                // //inserting into line_items
-                $lineItem = new LineItem();
-                $lineItem->order_id = $order->id;
-                $lineItem->product_id = $cartItem->id; 
-                $lineItem->name = $cartItem->name;
-                $lineItem->quantity = 1; 
-                $lineItem->unit_price = $cartItem->unit_price;
-                $lineItem->save(); //save line_items
-            }
-
-
-
-            //transaction vbox
-            $transaction = new _5bx(2257811, 'a88c8843898e4daad5646322ca06f22d');
-
-            $transaction->card_num($valid['cardNumber']);
-            $transaction->exp_date($valid['expiry']);
-            $transaction->cvv($valid['cvv']);
-            $transaction->card_type($valid['cardType']);
-            $transaction->ref_num($order->id);
+        $userdetails = Auth::user();
+        $order->user_id = Auth::id();
+        $order->address = $userdetails->address;
+        $order->city = $userdetails->city;
+        $order->province = $userdetails->province;
+        $order->postal_code = $userdetails->postal_code;
         
-            $response = $transaction->authorize_and_capture(); // returns JSON object
+        $order->save(); //save order
 
-            //save in transacation tables.
-        
-            $userdetails = Auth::user();
-        
-            // return view('invoice', compact('response', 'userdetails'));
 
-            $request->session()->forget('cart');
-        
-            return view('invoice', compact('response', 'userdetails'));
+        $cartItems = session('cartItems');
+        foreach($cartItems as $cartItem){
+            // //inserting into line_items
+            $lineItem = new LineItem();
+            $lineItem->order_id = $order->id;
+            $lineItem->product_id = $cartItem->id; 
+            $lineItem->name = $cartItem->name;
+            $lineItem->quantity = 1; 
+            $lineItem->unit_price = $cartItem->unit_price;
+            $lineItem->save(); //save line_items
         }
+        
+        if(!valid){
+            return redirect()->back()->withErrors($valid)->withInput($request->except('cardNumber', 'cvv', 'expiry'));
+        }
+
+        //transaction vbox
+        $transaction = new _5bx(2257811, 'a88c8843898e4daad5646322ca06f22d');
+
+        $transaction->card_num($valid['cardNumber']);
+        $transaction->exp_date($valid['expiry']);
+        $transaction->cvv($valid['cvv']);
+        $transaction->card_type($valid['cardType']);
+        $transaction->ref_num($order->id);
+    
+        $response = $transaction->authorize_and_capture(); // returns JSON object
+        // dd($response);
+        //save in transacation tables.
+
+    
+        $userdetails = Auth::user();
+
+        $request->session()->forget('cart');
+    
+        return view('invoice', compact('response', 'userdetails'));
+    
 
         // $cart = $request->session()->forget('cart');
         // return view('/invoice', compact('response','userdetails','cart'));
