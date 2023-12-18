9a989fd38b16953fe325ffa4e2b8b87f7bbd5ace
Medha <meduri-m@webmail.uwinnipeg.ca>
2023-12-18 11:33:19 -0600

    Edits made to admin/taxes/edit-changed dropdown to textbox

--- homeeleganz/resources/views/admin/taxes/edit.blade.php
+++ homeeleganz/resources/views/admin/taxes/edit.blade.php
@@ -34,19 +34,12 @@
 
                         <div class="sm:col-span-2">
                             <label for="pst" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">PST</label>
-                            <select id="pst" name="pst" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                <option selected disabled>Select PST tax rate</option>
-                                @foreach($filteredPstValues as $pstValue)
-                                        <option value="{{ $pstValue }}" 
-                                            @if($tax->pst === $pstValue) selected @endif>
-                                            {{ $pstValue }}
-                                        </option>
-                                @endforeach
-                            </select>
+                            <input type="number" step="0.01" name="pst" id="pst" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="PST Rate" value="{{ old('pst', $tax->pst) }}">
                             @error('pst')
                                 <span class="text-red-500">{{ $message }}</span>
                             @enderror
                         </div>
+
 
                         <div class="sm:col-span-2">
                             <label for="gst" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">GST</label>
