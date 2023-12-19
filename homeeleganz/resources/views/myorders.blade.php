b71308352df1ce84ad8d6512a10d1bda7e957e45
iamsahilr <srajbhandari63@gmail.com>
2023-12-19 09:08:11 -0600

    orders displaying in user's profile

--- homeeleganz/resources/views/myorders.blade.php
+++ homeeleganz/resources/views/myorders.blade.php
@@ -1,18 +1,30 @@
 @extends('layouts.main')
 @section('content')
 
-
+<div class="order-container">
+<h1 class="font-bold">Your Orders</h1>
 @if($orders->count() > 0)
-    <h1>Your Orders</h1>
     <ul>
         @foreach($orders as $order)
-            <li>{{ $order->id }} - {{ $order->name }} - {{ $order->description }}</li>
+            <li>
+                Order ID: {{ $order->id }}
+                <ul>
+                    @foreach($order->lineItems as $lineItem)
+                        <li>
+                            <img src="/images/{{ $lineItem->product->image }}" width="200px" alt="{{ $lineItem->product->name }}">
+                        </li>
+                        <li>
+                            Product Name: {{ $lineItem->product->name }}
+                        </li>
+                    @endforeach
+                </ul>
+            </li>
         @endforeach
     </ul>
 @else
-    <p>No orders found.</p>
+    <p>You have no orders yet.</p>
 @endif
-
+</div>
 
 @include('partials.footer')
 @endsection
