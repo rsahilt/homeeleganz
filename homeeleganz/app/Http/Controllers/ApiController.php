5d33d8058ebcb816b4ab99e71ef3335609b228d5
Shishir Shrestha <shishir.shrestha9808@gmail.com>
2023-12-21 00:16:59 +0545

    working on docblock comments

--- homeeleganz/app/Http/Controllers/ApiController.php
+++ homeeleganz/app/Http/Controllers/ApiController.php
@@ -26,9 +26,7 @@ class ApiController extends Controller
         
         ///inserting into order table
         $order = new Order();
-        $order->order_date = now();
-        // $order->sub_total = $valid['sub_total'];
-        //$order->pst = session('totalGST'); 
+        $order->order_date = now(); 
         $order->pst = session('totalPST'); 
         $order->gst = session('totalGST');
         $order->hst = session('totalHST');
@@ -50,9 +48,7 @@ class ApiController extends Controller
             if (isset($cartItem['product']) && is_array($cartItem['product']) && isset($cartItem['product']['id'])) {
                 $lineItem = new LineItem();
                 $lineItem->order_id = $order->id;
-                $lineItem->product_id = $cartItem['product']['id'];
-        
-                
+                $lineItem->product_id = $cartItem['product']['id'];            
                 $lineItem->name = $cartItem['product']['name'];
                 $lineItem->quantity = $cartItem['quantity'];
                 $lineItem->unit_price = $cartItem['product']['unit_price'];
@@ -60,10 +56,7 @@ class ApiController extends Controller
                 $lineItem->save(); // Save line_items
             } 
         }
-        
-        
-        
-
+           
         //transaction vbox
         $transaction = new _5bx(2257811, 'a88c8843898e4daad5646322ca06f22d');
 
